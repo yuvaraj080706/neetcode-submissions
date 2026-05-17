45a3d0fe0a47b0eca0499ae613d510be4e62c74c
Yuvaraj B E <161214187+yuvaraj080706@users.noreply.github.com>
2026-05-17 18:04:12 +0530

    Delete SQL directory

--- SQL/sql-capitalization/submission-3.sql
+++
@@ -1,9 +0,0 @@
-create TABLE users (
-  name Text
-);
-
-
--- Do not modify below this line --
-SELECT table_name, column_name, data_type
-FROM information_schema.columns
-WHERE table_name = 'users';
